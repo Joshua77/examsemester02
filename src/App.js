7f836b8d1e22a55613964bd44178511ba8c95ad2
Joshua77 <jpakinola@gmail.com>
2022-11-03 17:58:56 +0100

    First Phase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
+
+
   return (
-    <div className="App">
-      <div> Counter </div>
-      
+    <div className="app">
+      <div>
+        <div class="count">
+          <h3>Count:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div class="buttons">
+          <button onClick={incrementCount}> Increment </button>
+          <button onClick={decrementCount}> Decrement </button>
+          <button onClick={() => setCount(0)}> Reset </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
