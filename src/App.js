e32ce968f410f9370ca8794bc41ecb8cdb21d6d9
Joshua77 <jpakinola@gmail.com>
2022-11-03 18:33:27 +0100

    Styled the Buttons

--- src/App.js
+++ src/App.js
@@ -16,16 +16,18 @@ export default function App() {
 
   return (
     <div className="app">
+      <div className='page-design'>
       <div>
         <div class="count">
           <h3>Count:</h3>
           <h1>{count}</h1>
         </div>
         <div class="buttons">
-          <button onClick={incrementCount}> Increment </button>
-          <button onClick={decrementCount}> Decrement </button>
-          <button onClick={() => setCount(0)}> Reset </button>
+          <button className = "button-3" onClick={incrementCount}> Increment </button>
+          <button className = "button-2" onClick={decrementCount}> Decrement </button>
+          <button className = "button-1" onClick={() => setCount(0)}> Reset </button>
         </div>
+      </div>
       </div>
     </div>
   );
