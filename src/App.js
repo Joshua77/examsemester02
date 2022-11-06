17b947c1713cb7f7b433197df8e5ba5d0e9dc50b
Joshua77 <jpakinola@gmail.com>
2022-11-06 23:14:08 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Helmet } from "react";
+import { Helmet } from "react-helmet";
 //import { Links, Route, BrowserRoutes } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
