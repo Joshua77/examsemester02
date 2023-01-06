b92e40e225d62bff06467f33b83d15758327ed21
Joshua77 <jpakinola@gmail.com>
2023-01-06 04:32:43 +0100

    More changes

--- src/App.js
+++ src/App.js
@@ -1,82 +1,11 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
-//import { Links, Route, BrowserRoutes } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/Page404";
 
-//import { ErrorBoundary } from "react-error-boundary";
 
-//import Reducer from "./Components/usereducer.js";
-// import { ErrorBoundary } from "./Components/error.js";
-//import { NotFound } from "./Components/page404.js";
-//import { Fallback } from "./Components/Fallback.js";
-//import Reduce from "./components/usereducer.js";
-// import UseState from ".Components/useState.js";
-
-//NAVIGATION FUNCTION
-
-// CODE SECTION FOR THE REDUCER COMPONENT
-
-/*function ReducerPage(){
-  return(
-    <Reduce />
-  )
-
-}
-*/
-
-//CODE SECTION FOR THE ERROR BOUNDARY FUNCTION
-/*
-const ErrorHandler = (error, errorInfo)=>{
-  console.log('Logging', error, errorInfo)
-}
-
-function ErrorPage(){
-  return(
-    <ErrorBoundary FallBackCompnent={Fallback} onError = {ErrorHandler}>
-      <ClickCounter />
-    </ErrorBoundary>
-    
-  )
-}
-//ADD NAVIGATION ABOVE HERE
-
-
-
-// CODE SECTION FOR THE 404 PAGE
-
-function PageNotFound (){
-  return(
-    <NotFound />
-    
-  )
-
-}
-//ADD NAVIGATION above here
-
-*/
-
-// CODE SECTION FOR THE ROOT COMPONENT
-/*const MAX_COUNT_ALLOWED = 4
-export const  ClickCounter = () =>{
-  const [counter, setCounter] = useState(0);
-  //const handleError = useErrorHandler()
-}
-
-  const handleClick = ()=>{
-    try{
-        if(counter === MAX_COUNT_ALLOWED){
-            throw new Error('Count Limit Exceeded')
-        }
-        else {setCounter((c) => c+1)}
-    }
-    catch (e){
-        handleError(e)
-        //console.log("clickCounter error", e)
-    }}
-*/
 export default function App() {
   const [count, setCount] = useState(0);
 
